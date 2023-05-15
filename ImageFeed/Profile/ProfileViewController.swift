773fbe818c5bf272647216c60cc90e1843266088
Skoch33 <kochesem@gmail.com>
2023-05-15 13:55:24 +0300

    1. Сверстан экран профиля 2. Добавлен TapBar и TapBarController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Semen Kocherga on 15.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var discriptionLabel: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    
+    @IBAction func didTapLogOutButton() {
+    }
+}
