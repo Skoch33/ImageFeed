bd7c765791c99206f07bd9895e897439071a7d47
Skoch33 <kochesem@gmail.com>
2023-05-29 14:02:05 +0300

    1. Добавил экран авторизации 2. Добавил WebViewController для дальнейшей авторизации через OAuth 2.0

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Semen Kocherga on 26.05.2023.
+//
+
+import Foundation
+
+let AccessKey = "td64kmaBVOcrUR8dXMwRFgrS8HypQdQiA6vMYlmE5Io"
+let SecretKey = "U3UPleNeyUnB5mMB56-NFfGHDbB_Xx4yMRVNZezLPWQ"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
