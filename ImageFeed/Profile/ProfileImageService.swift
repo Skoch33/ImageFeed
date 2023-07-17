710ba44246819e83bce7b89b0be856c73758afb2
Skoch33 <kochesem@gmail.com>
2023-07-17 17:41:45 +0300

    Рефакторинг ProfileView и тесты к нему

--- ImageFeed/Profile/ProfileImageService.swift
+++ ImageFeed/Profile/ProfileImageService.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 final class ProfileImageService {
+    static let DidChangeNotification = Notification.Name(rawValue: "ProfileImageProviderDidChange")
     static let shared = ProfileImageService()
-    private init() {}
-    private (set) var avatarURL: URL?
+    private let urlSession = URLSession.shared
     private var getProfileImageTask: URLSessionTask?
     private let storageToken = OAuth2TokenStorage()
-    private var lastProfileImageCode: String?
-    let DidChangeNotification = Notification.Name(rawValue: "ProfileImageProviderDidChange")
+    private (set) var avatarURL: String?
     
     
     private enum GetProfileImageError: Error {
@@ -30,33 +29,37 @@ final class ProfileImageService {
             case profileImage = "profile_image"
         }
     }
+    
+    struct ProfileImage: Codable {
+        let profileImage: [String:String]
 
-    func fetchProfileImageURL(username: String, _ completion: @escaping (Result<URL, Error>) -> Void) {
+        init(decodedData: UserResult) {
+            self.profileImage = decodedData.profileImage
+        }
+    }
+
+    func fetchProfileImageURL(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
         assert(Thread.isMainThread)
-        
+
         let request = makeProfileImageRequest(token: storageToken.token!, username: username)
-        
         let session = URLSession.shared
-        getProfileImageTask = session.objectTask(for: request) { [weak self] (result: Result<UserResult, Error>) in
-            guard let self else { return }
-            self.getProfileImageTask = nil
+        let task = session.objectTask(for: request) { [weak self] (result: Result<UserResult, Error>) in
+            guard let self = self else { return }
             switch result {
-            case .success(let userResult):
-                guard let avatarStringURL = userResult.profileImage["large"],
-                      let avatarURL = URL(string: avatarStringURL) else {
-                    completion(.failure(GetProfileImageError.noURL))
-                    return
-                }
-                self.avatarURL = avatarURL
-                NotificationCenter.default.post(name: self.DidChangeNotification, object: nil)
-                completion(.success(avatarURL))
-            case .failure(_):
-                completion(.failure(GetProfileImageError.unableToDecodeStringFromProfileImageData))
-                self.lastProfileImageCode = nil
-                return
+            case .success(let decodedObject):
+                let avatarURL = ProfileImage(decodedData: decodedObject)
+                self.avatarURL = avatarURL.profileImage["large"]
+                completion(.success(self.avatarURL!))
+                NotificationCenter.default.post(
+                    name: ProfileImageService.DidChangeNotification,
+                    object: self,
+                    userInfo: ["URL": self.avatarURL!])
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
-        getProfileImageTask?.resume()
+        self.getProfileImageTask = task
+        task.resume()
     }
     
     private func makeProfileImageRequest(token: String, username: String) -> URLRequest {
